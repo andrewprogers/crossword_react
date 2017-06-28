4feb7c8c7700439fd35270c72e0c3c4c761c6407
Andrew Rogers <andrewprogers@gmail.com>
2017-06-28 12:35:07 -0400

    refactor clues container to const

--- src/components/CluesContainer.js
+++ src/components/CluesContainer.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import Clues from './Clues';
 
-class CluesContainer extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    return(
-      <div id='clues-container'>
-        <Clues
-          type='across'
-          clues={this.props.clues.across}
-          />
-        <Clues
-          type='down'
-          clues={this.props.clues.down}
-          />
-      </div>
-    )
-  }
+const CluesContainer = props => {
+  return(
+    <div id='clues-container'>
+      <Clues
+        type='across'
+        clues={props.clues.across}
+        />
+      <Clues
+        type='down'
+        clues={props.clues.down}
+        />
+    </div>
+  )
 }
 
 export default CluesContainer;
