d4689f81862627b3fc2cb98b8cfef1a8abc3d44b
Andrew Rogers <andrewprogers@gmail.com>
2017-06-30 17:06:28 -0400

    install jasmine

---
+++ spec/modules/CrosswordSpec.js
@@ -0,0 +1,13 @@
+import Crossword from '../../src/modules/Crossword';
+
+describe('Crossword', () => {
+  describe('.generateEmptyGrid', () => {
+    beforeEach(() => {
+      emptyArray = Crossword.generateEmptyGrid(4);
+    })
+
+    xit('creates a square array of arrays, where the contents are empty string', () => {
+
+    })
+  })
+})
